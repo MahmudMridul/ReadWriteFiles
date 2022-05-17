a7aab948bbd590a77009e1fe7cf8bcaf10cf9996
Abdullah Al Mahmud <mahmudmridul.185@gmail.com>
2022-05-17 22:02:26 +0600

    Write and Append text file

--- text_file.py
+++ text_file.py
@@ -2,6 +2,8 @@ text_file = 'text_file.txt'
 reading_mode = 'r'
 writing_mode = 'w'
 appending_mode = 'a'
+newline = '\n'
+lines = ['first line', 'quick brown fox', 'did not jump', 'a lazy dog is not lazy', 'what are these']
 
 def read_file(filedir):
     file = open(filedir, reading_mode)
@@ -25,3 +27,32 @@ def read_aslist(filedir):
     for line in lines:
         print(line)
     file.close()
+    
+def overwrite_with_singleline(filedir, lines):
+    file = open(filedir, writing_mode)
+    file.writelines(lines) #takes list of string and writes in single line
+    file.close()
+
+def overwrite_with_multiline(filedir, lines):
+    file = open(filedir, writing_mode)
+    for line in lines:
+        line += newline
+        file.write(line) #takes one string and writes
+    file.close()
+
+
+def append_in_singleline(filedir, lines):
+    file = open(filedir, appending_mode)
+    file.writelines(lines) #takes list of string and writes in single line
+    file.close()
+
+def append_in_multiline(filedir, lines):
+    file = open(filedir, appending_mode)
+    for line in lines:
+        line += newline
+        file.write(line) #takes one string and writes
+    file.close()
+
+append_in_multiline(text_file, lines)
+read_file(text_file)
+    
